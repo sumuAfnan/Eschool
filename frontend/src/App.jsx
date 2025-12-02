df6f7be6ab41824406461f4919ca28ee6f1dc23b
Sumaiya Akter <samm181075@gmail.com>
2025-12-02 07:03:14 +0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+   <h1 className="text-4xl font-bold text-blue-500">
+      Hello React with Tailwind!
+    </h1>
+  )
+}
+
+export default App
